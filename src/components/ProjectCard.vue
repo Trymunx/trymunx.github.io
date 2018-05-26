7a5cec1dd76d8a6fd85858b8a9836fe01c6b4392
Trymunx <trymunx@gmail.com>
2018-05-26 23:32:21 +0100

    Add project cards

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="project-card">
+    <slot name="card-image" class="card-image"></slot>
+    <slot name="project-title" class="project-title"></slot>
+    <slot class="card-content"></slot>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.project-card {
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0 4px 8px 0 #0000000A, 0 6px 20px 0 #00000032;
+}
+.card-image {
+  max-width: 100%;
+  height: auto;
+}
+
+.project-title {
+  font-size: 1.2em;
+  text-align: center;
+  padding: 10px;
+}
+.card-content {
+  text-align: left;
+  padding: 2px;
+}
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,55 @@
 <template>
-  <div class="projects">
+  <div>
+    <div class="header-box"></div>
+    <div class="projects">
+      <project-card>
+      <!-- <project-card class="project-card"> -->
+        <img src="@/assets/Spindelspel.png" slot="card-image" class="card-image">
+        <h1 slot="project-title" class="project-title">Spindelspel</h1>
+        <p class="card-content">This is a description of the game</p>
+        <!-- <p slot="card-content" class="card-content">This is a description of the game</p> -->
+      </project-card>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import ProjectCard from "@/components/ProjectCard";
+
+export default {
+  components: {
+    ProjectCard
+  }
+};
 </script>
 
 <style>
+.projects {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.project-card {
+  max-width: 600px;
+  padding-bottom: 5px;
+}
+
+.header-box {
+  height: 60px;
+}
+
+.card-image {
+  max-width: 100%;
+  height: auto;
+}
+
+.project-title {
+  font-size: 1.8em;
+  text-align: center;
+  padding: 10px;
+}
+.card-content {
+  text-align: left;
+  padding: 10px;
+}
 </style>
