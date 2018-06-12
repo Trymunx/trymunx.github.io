0fc82cbc62d14677b4ae87ea4f239575009e177d
Trymunx <trymunx@gmail.com>
2018-06-12 22:13:53 +0100

    Update project page to display cards programatically

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="project-card">
-    <slot name="card-image" class="card-image"></slot>
-    <slot name="project-title" class="project-title"></slot>
-    <slot class="card-content"></slot>
+    <slot name="card-image"/>
+    <div class="card-title">
+      <div class="project-title">
+        <slot name="project-title"/>
+      </div>
+      <span class="link-button">
+        <slot name="link-button"/>
+      </span>
+      <span class="source-button">
+        <slot name="source-button" class="source-button"/>
+      </span>
+    </div>
+    <slot class="card-content"/>
   </div>
 </template>
 
@@ -14,28 +24,78 @@ export default {};
 .project-card {
   display: flex;
   width: 45%;
-  margin: 10px;
+  max-width: 600px;
+  margin: 30px;
   padding-bottom: 5px;
   flex-grow: 1;
   flex-direction: column;
   border-radius: 4px;
+  background: #fff;
   box-shadow: 0 4px 8px 0 #0000000a, 0 6px 20px 0 #00000032;
 }
+
 .card-image {
   max-width: 100%;
   height: auto;
+  cursor: pointer;
+}
+
+.card-title {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
 }
 
 .project-title {
   font-size: 1.2em;
-  text-align: center;
+  padding: 14px 20px;
+  margin-right: auto;
 }
+
+.link-button > a {
+  padding: 8px 28px;
+
+  cursor: pointer;
+  border: none;
+  border-radius: 8px;
+  font-size: 1.2em;
+  text-decoration: none;
+  color: #ebf4eb;
+  background: #4c7553;
+  transition: all 0.35s ease;
+}
+
+.link-button > a:hover {
+  background: #4f935b;
+  box-shadow: 0 4px 8px 0 #0000001a, 0 6px 20px 0 #00000052;
+}
+
+.source-button > a {
+  font-size: 2em;
+  padding: 2px 10px;
+  margin: 10px 18px;
+  color: #2a3a2d;
+  transition: color 0.35s ease;
+}
+
+.source-button > a:hover {
+  color: #4c7553;
+}
+
 .card-content {
+  text-align: left;
+  padding: 2px 14px 8px;
+}
+
+.page-container {
+  background: #fafafa;
+  min-height: 100vh;
 }
 
 @media only screen and (max-width: 900px) {
   .project-card {
     width: 95%;
+    max-width: 600px;
   }
 }
 </style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,24 +1,13 @@
 <template>
-  <div class="page-container">
+  <div class="page-background">
     <div class="header-box"></div>
     <div class="projects">
-      <project-card>
-        <img src="@/assets/Spindelspel.png" slot="card-image" class="card-image">
-        <div slot="project-title" class="card-title">
-          <h1 class="project-title">Spindelspel</h1>
-          <a class="play-link" href="https://trymunx.github.com/Spindelspel" target="_blank">PLAY</a>
-          <a href="https://github.com/trymunx/Spindelspel" target="_blank" class="github-link"><i class="fab fa-github"></i></a>
-        </div>
-        <p class="card-content">A game written in <a href="https://p5js.org">p5.js</a>, a JavaScript port of Processing. The aim was to learn the p5js library by writing a game over a weekend. Most of the game was written over the course of two days, although the code isn't pretty. Making the spider assets was fun, but I never got around to making a player asset or adding a start page, which might have helped to explain the slightly quirky gameplay.</p>
-      </project-card>
-      <project-card>
-        <img src="@/assets/Spindelspel.png" slot="card-image" class="card-image">
-        <div slot="project-title" class="card-title">
-          <h1 class="project-title">Spindelspel</h1>
-          <a class="play-link" href="https://trymunx.github.com/Spindelspel" target="_blank">PLAY</a>
-          <a href="https://github.com/trymunx/Spindelspel" target="_blank" class="github-link"><i class="fab fa-github"></i></a>
-        </div>
-        <p class="card-content">A game written in <a href="https://p5js.org">p5.js</a>, a JavaScript port of Processing. The aim was to learn the p5js library by writing a game over a weekend. Most of the game was written over the course of two days, although the code isn't pretty. Making the spider assets was fun, but I never got around to making a player asset or adding a start page, which might have helped to explain the slightly quirky gameplay.</p>
+      <project-card v-for="(project, i) in projects" :key="i">
+        <img :src="project.img" slot="card-image" class="card-image">
+        <h1 slot="project-title">{{ project.title }}</h1>
+        <a slot="link-button" :href="project.link.url" target="_blank">{{ project.link.title }}</a>
+        <a slot="source-button" :href="project.source" target="_blank"><i class="fab fa-github"></i></a>
+        <p class="card-content">{{ project.description }}</p>
       </project-card>
     </div>
   </div>
@@ -26,8 +15,14 @@
 
 <script>
 import ProjectCard from "@/components/ProjectCard";
+import ProjectsJSON from "@/projects/index.json";
 
 export default {
+  data() {
+    return {
+      projects: ProjectsJSON
+    };
+  },
   components: {
     ProjectCard
   }
@@ -38,6 +33,7 @@ export default {
 .projects {
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
   padding: 10px;
 }
 
@@ -45,66 +41,16 @@ export default {
   height: 60px;
 }
 
-.card-image {
-  max-width: 100%;
-  height: auto;
-}
-
-.card-title {
-  display: flex;
-  align-items: center;
-}
-
-.project-title {
-  font-size: 1.8em;
-  padding: 14px 20px;
-  margin-right: auto;
-}
-
-.play-link {
-  padding: 8px 28px;
-
-  cursor: pointer;
-  border: none;
-  border-radius: 8px;
-  font-size: 1.2em;
-  text-decoration: none;
-  color: #ebf4eb;
-  background: #4c7553;
-  transition: all 0.35s ease;
-}
-.play-link:hover {
-  background: #4f935b;
-  box-shadow: 0 4px 8px 0 #0000001a, 0 6px 20px 0 #00000052;
-}
-.card-content {
-  text-align: left;
-  padding: 2px 14px 8px;
-}
-
-.page-container {
-  background: #fafafa;
-  min-height: 100vh;
-}
 .page-background {
   position: absolute;
-  background: url("/forest2.jpg") no-repeat center fixed;
-  background-size: cover;
+  /* background: url("/forest2.jpg") repeat-y center;
+  background-blend-mode: lighten;
+  background-size: cover; */
+  background: linear-gradient(165deg, #fefefe 60%, #d3d3d3);
   z-index: -1;
   width: 100vw;
   max-width: 100%;
-  height: 100vh;
-  filter: opacity(0.8) saturate(1.6) contrast(1.2);
-}
-.github-link {
-  font-size: 2em;
-  padding: 2px 10px;
-  margin: 10px 18px;
-  color: #2a3a2d;
-  transition: color 0.35s ease;
-}
-
-.github-link:hover {
-  color: #4c7553;
+  min-height: 100vh;
+  /* filter: opacity(0.8) saturate(1.1) contrast(1.2); */
 }
 </style>
