7f4866bc50b5767ac555936c13f71054106434b1
Trymunx <trymunx@gmail.com>
2018-06-14 17:07:32 +0100

    Update about page for mobile

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,19 @@
 <template>
+  <div>
   <div class="about">
     <div class="contents">
-      <about-card>
+      <img src="@/assets/Alex-Line.png" class="profile-image">
+    </div>
+    <!-- <div class="about-me">
+      <h2>Alexander Houseago</h2>
+      <p>Hobbyist web developer and Linux enthusiast.</p>
+      <div class="social-links">
+        <a href="https://twitter.com/trymunx" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://github.com/trymunx" target="_blank"><i class="fab fa-github"></i></a>
+        <a href="mailto:trymunx@gmail.com"><i class="fas fa-envelope"></i></a>
+      </div>
+    </div> -->
+      <!-- <about-card>
         <img src="@/assets/Alex-Line.png" class="profile-image">    
         <h2>Alexander Houseago</h2>
         <p>Hobbyist web developer and Linux enthusiast.</p>
@@ -10,7 +22,17 @@
           <a href="https://github.com/trymunx" target="_blank"><i class="fab fa-github"></i></a>
           <a href="mailto:trymunx@gmail.com"><i class="fas fa-envelope"></i></a>
         </div>
-      </about-card>
+      </about-card> -->
+    <!-- </div> -->
+  </div>
+    <div class="about-me">
+      <h2>Alexander Houseago</h2>
+      <p>Hobbyist web developer and Linux enthusiast.</p>
+      <div class="social-links">
+        <a href="https://twitter.com/trymunx" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://github.com/trymunx" target="_blank"><i class="fab fa-github"></i></a>
+        <a href="mailto:trymunx@gmail.com"><i class="fas fa-envelope"></i></a>
+      </div>
     </div>
   </div>
 </template>
@@ -27,23 +49,57 @@ export default {
 </script>
 
 <style>
-.about {
+.contents {
   display: flex;
   justify-content: center;
   align-items: center;
-  background: url("/forest1.jpg") no-repeat center fixed;
+  background: url("/snow.jpg") no-repeat center fixed;
   background-size: cover;
-  height: 100vh;
+  height: 70vh;
+}
+.about {
+  display: flex;
+  flex-direction: column;
+  /* height: 92vh; */
+  /* height: 100vh; */
+  height: 100%;
+}
+
+.about-me {
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 30%;
+  width: 100vw;
+  max-width: 100%;
+  background: #fff;
+  box-shadow: 0 -4px 18px 0 #0000004d;
+}
+
+.about-me > * {
+  padding: 8px 2px;
+}
+
+.about-me > p {
+  padding: 8px 18px;
 }
 
 .profile-image {
-  width: 160px;
+  /* width: 160px; */
+  width: 50%;
+  max-width: 400px;
   transition: width 0.5s ease-in;
 }
 
 .profile-image:hover {
   width: 1000px;
   transition: all 200s cubic-bezier(0.95, 0.05, 0.795, 0.035);
+}
+
+.social-links {
+  margin-top: 20px;
 }
 
 .social-links > a {
