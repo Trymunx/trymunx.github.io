019274622e0d823a2873295a6c4afe0d62cb8c92
Trymunx <trymunx@gmail.com>
2018-11-05 19:49:49 +0000

    didn't commit this

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
+VERSION="$(cat version)"
+echo -n "Version ($VERSION): "
+read ver
+echo $ver > version
+
+git commit version -m "Update build version: $ver"
+
 # abort on errors
 set -e
 
@@ -14,7 +21,7 @@ echo 'trymunx.com' > CNAME
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "Deploy $ver"
 
 # if you are deploying to https://<USERNAME>.github.io
 git push -f git@github.com:trymunx/trymunx.github.io.git master
@@ -23,3 +30,4 @@ git push -f git@github.com:trymunx/trymunx.github.io.git master
 # git push -f git@github.com:trymunx/<REPO>.git master:gh-pages
 
 cd -
+git push
