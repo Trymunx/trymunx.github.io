ebbfed99ae5f872503c8b82ec10023b483da2183
Trymunx <trymunx@gmail.com>
2019-10-03 22:17:46 +0100

    start blog desktop layout

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="blog"></div>
+  <div class="blog-container">
+    <div class="blog-sidebar">
+      <div class="blog-link">
+        Blog post one
+      </div>
+      <div class="blog-link">
+        Blog post two
+      </div>
+      <div class="blog-link">
+        Blog post three
+      </div>
+    </div>
+    <div class="blog-main">
+      Blog post
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +24,30 @@ export default {
 </script>
 
 <style>
+.blog-container {
+  display: grid;
+  grid-template-areas: "sidebar main";
+  grid-template-columns: 400px 1fr; 
+  min-height: 100%;
+}
+
+.blog-main {
+  grid-area: "main";
+  padding: 20px;
+}
+
+.blog-sidebar {
+  background-color: #fafafa;
+  grid-area: "sidebar"
+}
+
+.blog-link {
+  cursor: pointer;
+  padding: 20px;
+  text-align: left;
+}
+
+.blog-link:hover {
+  background-color: #dde2dd;
+}
 </style>
